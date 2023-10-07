836d44507add24f404e0cd10a2baceed3140406c
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-07 11:57:19 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,58 @@
+function generateRandomData() {
+function main() {
+main();
+    data.forEach(item => {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    });
+    }
+
+}
+
+    const data = [];
+    return data;
+    const data = generateRandomData();
+
+        for (int item : data) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+    }
+        }
+        int[] data = new int[10];
+}
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {def main():
+
+
+if __name__ == "__main__":
+def generate_random_data():
+    return data
+import random
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    for item in data:    });
+    const data = generateRandomData();
+function main() {
+
+    return data;
+    const data = [];
+        console.log("Random Number: " + item);
+main();
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    }
+
+
+function generateRandomData() {
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
