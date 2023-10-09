311e5f77083d065a9d1fdfb622db8020e87fd659
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-09 18:01:32 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+def main():
+
+    main()
+if __name__ == "__main__":
+    return data
+
+    for item in data:
+
+import random    data = [random.randint(1, 100) for _ in range(10)]
+
+
+if __name__ == "__main__":
+def generate_random_data():
+
+
+def main():
+import random
+    for item in data:
+    return data
+    main()
+    data = generate_random_data()
+        print(f"Random Number: {item}")
