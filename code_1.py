d1b3c6720657797d565390023bcd123815e05488
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-16 12:13:00 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,71 +1,26 @@
-    });
-    data.forEach(item => {
-    const data = [];
-    const data = generateRandomData();
-        data.push(Math.floor(Math.random() * 100) + 1);
-        console.log("Random Number: " + item);
-function generateRandomData() {
-main();
-function main() {
-    return data;
-
-}
-
-
-}
-    }
-    for (let i = 0; i < 10; i++) {import random
-if __name__ == "__main__":
-def generate_random_data():
-    return data
 
     main()
+import random
+def generate_random_data():
 
-
-        print(f"Random Number: {item}")
-def main():
     data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+    data = generate_random_data()
+
+if __name__ == "__main__":
+    return data
+
+        print(f"Random Number: {item}")def generate_random_data():
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+import random
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
 
     for item in data:
     data = generate_random_data()
-    data.forEach(item => {
-function generateRandomData() {
-main();
-    for (let i = 0; i < 10; i++) {
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-    }
-    });
-
-    return data;
-    const data = generateRandomData();
-    const data = [];
-        console.log("Random Number: " + item);
-
-}
-}
-    main()
 
     return data
-if __name__ == "__main__":
-def generate_random_data():
-
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-import random        for (int i = 0; i < 10; i++) {
-}
-        int[] data = new int[10];
-        }
-    }
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-
-
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
