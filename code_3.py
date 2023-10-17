de4891eb0c93bbbd77a7606e5cd6e183a91d4b0b
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-10-17 15:15:01 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,14 @@
-def main():
 
 
 
-    data = generate_random_data()
-if __name__ == "__main__":
-import random
-    for item in data:
+
     main()
         print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
-    return data
+import random
+if __name__ == "__main__":
 def generate_random_data():
+    data = generate_random_data()
+    for item in data:
+    return data
+def main():
